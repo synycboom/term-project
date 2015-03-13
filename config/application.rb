020739da6aede1b3331e520836ad62b383e5d888
วุธิชัย  จันทร์สวัสดิ์ <synycboom@gmail.com>
2015-03-13 01:22:53 +0000

    disable precompile

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,9 @@ module TermProject
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
+    
+    config.assets.initialize_on_precompile = false
+    
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
